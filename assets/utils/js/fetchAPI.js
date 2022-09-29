0f1eab26c7abd1398d019f1ede29256fb9f8b682
gabriel-tobing <gabrieltobing789@gmail.com>
2022-09-29 16:53:00 +0700

    Menambahkan style untuk parameter

--- assets/utils/js/fetchAPI.js
+++ assets/utils/js/fetchAPI.js
@@ -1,14 +1,13 @@
 const cardContainer = document.getElementById("card-container");
 
 const fetchAPI = async () => {
+    let card = "";
     
     const API_KEY = "f497c4b7256042e9af6f4ccccfc458d1";
 
     const fetchData = await fetch(`https://api.spoonacular.com/recipes/random?number=4&apiKey=${API_KEY}`);
     const result = await fetchData.json();
     const datas = result.recipes;
-
-    let card = "";
 
     datas.forEach((data) => card += cardComponent(data));
 
@@ -18,48 +17,53 @@ const fetchAPI = async () => {
 const cardComponent = (data) => {
     if(data.dishTypes.length == 0) {
         return `
-        <div class="card-group">
-            <div class="card shadow-sm">
-                <img src="${ data.image }" class="card-img-top" alt="${ data.title }" style="height: 200px; object-fit: cover;">
+            <div class="card-group">
+                <div class="card shadow-sm ${ data.vegetarian ? "vegan" : "not-vegan" } ${ data.veryHealthy ? "healthy" : "not-healthy" }">
+                    <img src="${ data.image }" class="card-img-top" alt="${ data.title }" style="height: 200px; object-fit: cover;">
 
-                <div class="card-body pt-3 px-2">
-                    <h5 class="title card-title text-truncate">${ data.title }</h5>
+                    <div class="card-body pt-3 px-2">
+                        <h5 class="title card-title text-truncate">${ data.title }</h5>
 
-                    <div class="d-flex justify-content-between align-items-center flex-wrap mt-3">
-                        <div class="card-info">
-                            <i class="bi bi-stopwatch me-1"></i>
-                            <span>${ data.readyInMinutes } minutes</span>
+                        <div class="d-flex justify-content-between align-items-center flex-wrap mt-3">
+                            <div class="card-info">
+                                <i class="bi bi-stopwatch me-1"></i>
+                                <span>${ data.readyInMinutes } minutes</span>
+                            </div>
+                            
+                            <div>
+                                <svg viewBox="0 0 1000 200" class="rating">
+                                    <defs>
+                                        <polygon id="star" points="100,0 131,66 200,76 150,128 162,200 100,166 38,200 50,128 0,76 69,66 "/>
+                                        <clipPath id="stars">
+                                            <use xlink:href="#star"/>
+                                            <use xlink:href="#star" x="20%"/>
+                                            <use xlink:href="#star" x="40%"/>
+                                            <use xlink:href="#star" x="60%"/>
+                                            <use xlink:href="#star" x="80%"/>
+                                        </clipPath>
+                                    </defs>
+                                    <rect class="rating-background" clip-path="url(#stars)"></rect>
+                                    <rect width="${ (data.aggregateLikes / 5) * 20 }%" class="rating-value" clip-path="url(#stars)"></rect>
+                                </svg>
+                            </div>
                         </div>
-                        
-                        <div>
-                            <svg viewBox="0 0 1000 200" class="rating">
-                                <defs>
-                                    <polygon id="star" points="100,0 131,66 200,76 150,128 162,200 100,166 38,200 50,128 0,76 69,66 "/>
-                                    <clipPath id="stars">
-                                        <use xlink:href="#star"/>
-                                        <use xlink:href="#star" x="20%"/>
-                                        <use xlink:href="#star" x="40%"/>
-                                        <use xlink:href="#star" x="60%"/>
-                                        <use xlink:href="#star" x="80%"/>
-                                    </clipPath>
-                                </defs>
-                                <rect class="rating-background" clip-path="url(#stars)"></rect>
-                                <rect width="${ (data.aggregateLikes / 5) * 20 }%" class="rating-value" clip-path="url(#stars)"></rect>
-                            </svg>
-                        </div>
-                    </div>
 
-                    <div class="mt-4">
-                        <a href="${ data.id }" class="primary-button shadow-sm">Lihat Resep</a>
+                        <div class="d-flex align-items-center gap-2 mt-3">
+                            <div class="vegan-parameter" title="${ data.vegan ? "Cocok untuk Vegetarian" : "Tidak cocok untuk Vegetarian" }"></div>
+                            <div class="health-parameter" title="${ data.veryHealthy ? "Makanan yang menyehatkan" : "Tidak menyehatkan" }"></div>
+                        </div>
+
+                        <div class="mt-4">
+                            <a href="${ data.id }" class="primary-button shadow-sm">Lihat Resep</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    `
+        `
     } else {
         return `
             <div class="card-group">
-                <div class="card shadow-sm">
+                <div class="card shadow-sm ${ data.vegan ? "vegan" : "not-vegan" } ${ data.veryHealthy ? "healthy" : "not-healthy" }">
                     <img src="${ data.image }" class="card-img-top" alt="${ data.title }" style="width: 100%; height: 200px; object-fit: cover;">
 
                     <div class="card-body pt-3 px-2">
@@ -87,6 +91,11 @@ const cardComponent = (data) => {
                                     <rect width="${ (data.aggregateLikes / 5) * 20 }%" class="rating-value" clip-path="url(#stars)"></rect>
                                 </svg>
                             </div>
+                        </div>
+
+                        <div class="d-flex align-items-center gap-2 mt-3">
+                            <div class="vegan-parameter" title="${ data.vegan ? "Cocok untuk Vegetarian" : "Tidak cocok untuk Vegetarian" }"></div>
+                            <div class="health-parameter" title="${ data.veryHealthy ? "Makanan yang menyehatkan" : "Tidak menyehatkan" }"></div>
                         </div>
 
                         <div class="mt-4">
